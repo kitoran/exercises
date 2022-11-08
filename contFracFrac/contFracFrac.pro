8313cfaca9f4adad3883e4a5d9d5a2ce50a885ba
kitoran <kitttoran@gmail.com>
2022-11-08 13:46:48 +0200

    contFracFrac compiles with new dual-backended gui

--- contFracFrac/contFracFrac.pro
+++ contFracFrac/contFracFrac.pro
@@ -2,10 +2,11 @@ TEMPLATE = app
 CONFIG += console
 CONFIG -= app_bundle
 CONFIG -= qt
-
+CONFIG += X
 guiPers = true;
-include(../../library/gui-X/gui.pri)
+include(../../library/gui/gui.pri)
 include(../../library/misc/misc.pri)
+include(../../library/stb/stb_ds.pri)
 QMAKE_CFLAGS += -Wno-pointer-sign
 QMAKE_CFLAGS +=  -Ofast
 DEFINES += MY_PATH=\\\"$$PWD\\\"
--- contFracFrac/main.c
+++ contFracFrac/main.c
@@ -1,12 +1,17 @@
 ﻿#include <stdio.h>
 #include <X11/Xlib.h>
+#define EXPOSE_X11_GLOBALS_PLEASE
 #include "gui.h"
+#include "math.h"
 #include "gridlayout.h"
 #include "persistent.h"
 #include "color.h"
 #include "complex.h"
 #define tau 6.283185307179586
-XEvent xEvent;
+//event event;
+//extern Display* xdisplay;
+//extern int xDepth;
+
 //char appName[] = "contfracfrac";
 //int getColor(int blue, int red) {
 //    // or = rb*b + rr*r + ofr
@@ -15,6 +20,7 @@ XEvent xEvent;
 
 //    // 255 = rb*
 //}
+void recalculatePicture();
 void iter( double* restrict x, double* restrict  y) {
 //    *x = *x - floor(*x);
 //    *y = *y - floor(*y);
@@ -53,32 +59,32 @@ void numToPicDD (double x, double y,int * restrict rx, int * restrict ry) {
 }
 enum { itersMode, selectMode, zoominMode} mode;
 void loop(Painter* pa, bool* consume) {
-    Grid* g = topGrid();
-    setCurrentGridPos(g, 0,0);
+//    Grid* g = topGrid();
+    setCurrentGridPos(0,0);
 //    bool res = false;
     bool recalc = guiNumberEdit(pa, 5, &itNum, consume);
 
-    setCurrentGridPos(g,0,1);
+    setCurrentGridPos(0,1);
     if(resourseToolButton(pa, "minus.png", consume)) {
         itNum--;
         recalc = /*res = */true;
     }
-    setCurrentGridPos(g,0,2);
+    setCurrentGridPos(0,2);
     if(resourseToolButton(pa, "plus.png", consume)) {
         itNum++;
         recalc = /*res = */true;
     }
-    setCurrentGridPos(g,0,3);
+    setCurrentGridPos(0,3);
     if(resourseToolButton(pa, "select.png", consume)) {
         mode = selectMode;
 //        res = true;
     }
-    setCurrentGridPos(g,0,4);
+    setCurrentGridPos(0,4);
     if(resourseToolButton(pa, "iters.png", consume)) {
         mode = itersMode;
 //        res = true;
     }
-    setCurrentGridPos(g,0,5);
+    setCurrentGridPos(0,5);
     if(resourseToolButton(pa, "zoomin.png", consume)) {
         mode = zoominMode;
 //        res = true;
@@ -88,7 +94,7 @@ void loop(Painter* pa, bool* consume) {
 //        mode = zoominMode;
 ////        res = true;
 //    }
-    setCurrentGridPos(g,0,7);
+    setCurrentGridPos(0,7);
     if(resourseToolButton(pa, "100percent.png", consume)) {
         c = 1;
         xm = 0;
@@ -105,7 +111,7 @@ void loop(Painter* pa, bool* consume) {
 void recalculatePicture() {
     for(int i = 0; i < 600; i++) {
         for(int j = 0; j < 600; j++) {
-            bool in = false;
+//            bool in = false;
 //            ((int*)data)[i*600+j] =
             double xn,yn;
             picToNumDD(i, j, &xn, &yn);
@@ -172,7 +178,7 @@ const char* appName = "continuous fraction fractals";
 int main()
 {
     guiStartDrawing();
-    guiSetSize(rootWindow, size, size);
+    guiSetSize(size, size);
     recalculatePicture();
     XImage *res = XCreateImage(xdisplay, DefaultVisual(xdisplay, DefaultScreen(xdisplay)), 24,
                  ZPixmap, 0, data, size, size, 32,
@@ -191,20 +197,20 @@ int main()
         guiNextEvent();
         XPutImage(xdisplay, rootWindow, gc, res, 0, 0, 0,0, 600, 600);
         volatile int t;
-        if(xEvent.type == ButtonPress) {
+        if(event.type == ButtonPress) {
            fprintf(stderr, "hi%d", t);
         }
         bool consume = false;
         loop(&pa, &consume);
         printf("%d", consume);
-        if(xEvent.type == DestroyNotify) {
+        if(event.type == DestroyNotify) {
             goto exit;
         };
         if(!consume && mode == itersMode) {
-            if(xEvent.type == ButtonPress || xEvent.type == MotionNotify) {
+            if(event.type == ButtonPress || event.type == MotionNotify) {
                 double x, y;
                 double xn, yn;
-                picToNumDD(xEvent.xbutton.x, xEvent.xbutton.y, &x, &y);
+                picToNumDD(event.xbutton.x, event.xbutton.y, &x, &y);
                 for(int i = 0; i < 10; i++) {
                     xn = x; yn = y;
                     iter(&x, &y);
@@ -218,31 +224,31 @@ int main()
             };
         } else if(!consume && mode == selectMode) {
             static int startx, starty;
-            static bool sel = false;
-            if(xEvent.type == ButtonPress) {
-                startx = xEvent.xbutton.x;
-                starty = xEvent.xbutton.y;
-                sel = true;
-            } else if(xEvent.type == ButtonRelease) {
+//            static bool sel = false;
+            if(event.type == ButtonPress) {
+                startx = event.xbutton.x;
+                starty = event.xbutton.y;
+//                sel = true;
+            } else if(event.type == ButtonRelease) {
                 double sx, sy;
                 picToNumDD(startx, starty,
                          &sx, &sy);
                 double ex, ey;
-                picToNumDD(xEvent.xbutton.x, xEvent.xbutton.y,
+                picToNumDD(event.xbutton.x, event.xbutton.y,
                          &ex, &ey);
 #define MAX(x,y) ((x)>(y)?(x):(y))
 #define MIN(x,y) ((x)<(y)?(x):(y))
                 c = MAX(fabs(ex-sx), fabs(ey-sy));
                 xm = -MIN(sx,ex);
                 ym = -MIN(sy,ey);
-                sel = false;
+//                sel = false;
                 recalculatePicture();
             }
 
         } else if(!consume && mode == zoominMode) {
-            if(xEvent.type == ButtonRelease) {
-                double mouseX = xEvent.xbutton.x;
-                double mouseY = xEvent.xbutton.y;
+            if(event.type == ButtonRelease) {
+                double mouseX = event.xbutton.x;
+                double mouseY = event.xbutton.y;
                 /*
                  * mouseX * c / 2 - xmnew = mouseX * c - xmold
                  * mouseY * c / 2 - ymnew = mouseY * c - ymold
@@ -257,7 +263,7 @@ int main()
                  * xmnew = xmold - mouseX*c + c/2
                  */
                 double cxcx = mouseX * c/size - xm;
-                double newxm, newym;
+//                double newxm, newym;
                 double cxcxoldx, d; picToNumDD((int)mouseX, (int)mouseY, &cxcxoldx, &d);
 //                picToNum(mouseX - size/4,
 //                         mouseY - size/4,
